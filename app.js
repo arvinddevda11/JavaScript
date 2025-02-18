bb7f4b979bb950d35758f94a059aa324acc9e1a4
arvinddevda11 <arviipal42@gmail.com>
2025-02-18 23:59:15 +0530

    createElement

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-function addOptiLanguage(langName){
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(langName));
-    document.querySelector('.language').appendChild(li)
-    }
-    addOptiLanguage('golang')
+  const div = document.createElement('div')
+    console.log(div)
+    div.className= 'firstName'
+    div.setAttribute("title", "generated title")
+    div.style.backgroundColor= 'orange'
+    
+    div.style.color="red"
+    div.style.width = "200px"
+    div.style.borderRadius= "15px"
+    div.style.fontSize= "25px"
+    div.style.fontWeight= "bold"
+
+    div.style.padding='15px'
+    const addText = document.createTextNode("Hello Everyone")
+     div.appendChild(addText)
+     
+    document.body.appendChild(div)
    
