678b3855c2bfcffe409c464b2dd8bbf1054877db
arvinddevda11 <arviipal42@gmail.com>
2025-02-19 21:18:39 +0530

    add-more

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ changeNameEle.textContent = 'Mojo'
 thirdName.replaceWith(changeNameEle)
 
 
+
+function addOptiLanguage(langName){
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(langName));
+    document.querySelector('.language').appendChild(li)
+
+    }
+    addOptiLanguage('golang')
+
+    
+const changeName = document.querySelector("li:last-child")
+const nameRep = document.createElement('li')
+nameRep.textContent = 'HTML'
+changeName.replaceWith(nameRep)
+
+
