a625e82099c211e8106e3c94c8bd873d80cbf500
arvinddevda11 <arviipal42@gmail.com>
2025-03-08 13:33:21 +0530

    type

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-
-function addElement(langName){
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(langName));
-    document.querySelector('.language').appendChild(li)
-
-}
-addElement('CSS')
-
-function addOptiLanguage(langName){
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(langName));
-    document.querySelector('.language').appendChild(li)
-
-    }
-    addOptiLanguage('Golang')
-
-const repName = document.querySelector("li:first-child")
-const elementName = document.createElement('li')
-elementName.textContent = "Java"
-repName.replaceWith(elementName)
-
-const thirdName = document.querySelector("li:nth-child(4)")
-const changeNameEle = document.createElement('li')
-changeNameEle.textContent = 'Mojo'
-thirdName.replaceWith(changeNameEle)
-
+const profile = {
+    userName : "Arvind",
+    isFollow : true,
+    about : "learning coding",
+    followers : 432
+  };                              
+  console.log(typeof profile["isFollow"])
